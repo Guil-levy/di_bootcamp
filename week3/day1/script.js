f98b2438163d0f2b3d583471c2c37db37820b439
Guil-levy <guil.levy12@gmail.com>
2023-07-23 19:43:29 +0300

    new commit 23072023 1943

---
+++ week3/day1/script.js
@@ -0,0 +1,63 @@
+// //VARIABLES
+
+// let name ='John';
+// // const email;
+// // var username;
+// console.log(name);
+// name= 'Marry';
+
+// console.log(name);
+
+// //const
+// const COLOR = "red";
+// console.log(COLOR);
+
+// // // JS Data types
+// // 1.String
+// // 2.Number
+// // 3.Boolean
+
+// 1/ String
+let first_name = "hello World";
+let last_name = " planete Earth";
+let full_name = first_name + " " + last_name;
+// console.log(full_name);
+
+// String methods
+//length
+// console.log(full_name.length);
+//indexOf
+// let pos = full_name.indexOf("h")
+// console.log(pos);
+
+// // //search method
+// let pos= full_name.search("planete");
+// console.log(pos);
+
+// //slice
+// let str = "Annanas, Kiwi, Apple" 
+// let res = str.slice(8);
+// console.log (res);
+
+//substring   substr
+
+//replace
+
+// let str = "Please visit Microsoft";
+// let new_str = str.replace("Microsoft","Developers Institute");
+// console.log(new_str);
+
+// // toUpperCase(). toLowerCase()
+// console.log(str.toLowerCase());
+// console.log(str.toUpperCase());
+
+//ltrim   rtrim   trim
+// let str = "     Hello     ";
+// console.log(str.trimRight());
+
+let addressNumber = '2';
+let addressStreet = "Dam hamaccabim";
+let country = "Israel"
+
+let globalAdress = addressNumber + " "+ addressStreet+ ","+ country;
+console.log("I leave in "+ globalAdress);
