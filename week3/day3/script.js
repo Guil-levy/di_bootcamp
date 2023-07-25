98f834b3f56466ba9a69c98d2e7fa786488c2ffd
Guil-levy <guil.levy12@gmail.com>
2023-07-25 20:00:51 +0300

    A

--- week3/day3/script.js
+++ week3/day3/script.js
@@ -1,1 +1,56 @@
-console.log( "hello world")
+// COnditions and Objects
+
+// Conditionals
+// If, else if, else, switch
+
+// let x=5;
+// let y=6;
+
+// if(x>y){ 
+//     console.log( "hello world");}
+// else if(false){
+//     console.log('equal to 6');
+// }
+// else{
+//     console.log('oups i am the default');
+// }
+
+// console.log('bla bla');    
+
+// Exercice1
+
+// let age = prompt('What is your age?');
+// console.log(age);
+
+// if(age<18){
+//     alert('Sorry, you are too young to drive this car. Powering off');
+
+// }
+// else if (age==18) {
+//     alert('Congratulations on your first year of driving, Enjoy the ride!');
+// }
+//     else  {
+//         alert('Powering on. Enjoy the ride!');
+// } 
+// End of exercice
+
+// const Driver_AGE= 18;
+// let age = prompt('Please enter your age');
+// if(age<Driver_AGE){
+//     alert('sorry');
+// }
+// else if(age == Driver_AGE){alert('Congratulation');}
+// else{alert('Powering on');}
+
+
+
+
+// Logical Operators ( && and, || or, ! not)
+let a=1;
+let b=2;
+let c=3;
+let d=3;
+
+if (a===1 && c===d){
+
+}
