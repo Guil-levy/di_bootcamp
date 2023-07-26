c803c7cfb120f6d25a24fd3b424cd70857ab3452
Guil-levy <guil.levy12@gmail.com>
2023-07-26 17:01:34 +0300

    a

--- week3/day3/script.js
+++ week3/day3/script.js
@@ -46,11 +46,67 @@
 
 
 // Logical Operators ( && and, || or, ! not)
-let a=1;
-let b=2;
-let c=3;
-let d=3;
+// let a=1;
+// let b=2;
+// let c=3;
+// let d=3;
 
-if (a===1 && c===d){
+// if (a===1 && c===d){
 
-}
+// }
+
+// Switch Statement
+ 
+// switch ('about') {
+//     case 'about':
+//         alert('Welcome to about page')
+        
+//         break;
+//     case 'home':
+//             alert('Welcome to home page')
+            
+//         break;
+
+//     default:
+//         alert('404 page')
+//         break;
+// }
+
+
+// // OBJET
+
+
+// let user = {
+//     name: 'John',
+//     email:'john@gmail.com',
+//     age: 18,
+//     fav: ['Apple', 'Banana'],
+//     address : {
+//         city:'Tel Aviv',
+//         country: 'Israel'
+//     },
+//     // calc: fonction(){}
+// }
+
+// // console.log(user.name);
+// // console.log(user['email']);
+// user.name = 'johny';
+// user['email']= 'johny@gmail.com';
+// console.log(user);
+
+let user1 = {
+    username:'Guil',
+    password: 'aaa'
+};
+let database = [user1];
+let newsfeed = [
+    {username: 'user2',
+    timeline:'date1',},
+    {username: 'user3',
+        timeline:"date2"},
+    {username:'user4',
+        timeline:'date3'}
+];
+console.log(user1);
+console.log(database);
+console.log(newsfeed);
