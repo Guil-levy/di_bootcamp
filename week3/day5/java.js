a58682210963a75346f6e85d7345f4a2473fb8e0
Guil <guil.levy12@gmail.com>
2023-07-27 19:21:55 +0300

    loops

---
+++ week3/day5/java.js
@@ -0,0 +1,34 @@
+// for( let i=0; i<10; i++){
+//     console.log("the current number " + i);
+// }
+
+
+// let arr = [1, 4, 7, 10]
+// for (let i=0; i<arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// Exercice1
+
+// for (let i = 0; i < 16; i++) {
+    
+//     if (i % 2 === 0) {  
+//         console.log(i + " is even");
+//     }
+//     else{
+//         console.log(i + " is odd");
+//     }
+// }
+
+//Looping in OBJECT
+// const person = {fname:"John", lname:"Doe", age:25};
+// for (let x in person) {
+//   console.log(x) //fname lname age
+//   console.log(person[x]) // John Doe 25
+// }
+
+// const colors = ["Blue", "Pink", "Green", "Purple"];
+// for (let i of colors) {
+//     console.log(i);
+    
+// }
