8866782e333c52e5dad806efafc1373e2734d612
Guil-levy <guil.levy12@gmail.com>
2023-07-29 16:41:31 +0300

    day4-dayli

---
+++ week3/day4/dailyChalenge/script.js
@@ -0,0 +1,26 @@
+// 1/
+
+let sentence = "The movie is not that bad, I like it";
+console.log(sentence);
+
+// 2/
+
+const indexOfNot = sentence.indexOf("not");
+console.log(indexOfNot);
+
+// 3/
+
+const indexOfBad = sentence.indexOf("bad");
+console.log(indexOfBad);
+
+// 4/
+
+
+if( indexOfNot < indexOfBad ){
+    let sentence = "The movie is good, i like it."
+    console.log(sentence);
+}
+// 5/
+else{
+ console.log(sentence);
+}
