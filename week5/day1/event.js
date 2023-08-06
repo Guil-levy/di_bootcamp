036733ae8c8a879703f1c6253ecb376a3143900c
Guil-levy <guil.levy12@gmail.com>
2023-08-06 22:03:13 +0300

    1

--- week5/day1/event.js
+++ week5/day1/event.js
@@ -1,34 +1,33 @@
-function handleClick () {
-    // console.log('buttom was clicked');
-    const div = document.getElementById('innerDiv');
-    const button = document.createElement('button');
-    button.textContent= "Click me!!!";
-    
-    div.appendChild(button);
-    
-    button.addEventListener('click', function (event) {
-        console.log(event.target);
-    })
-    const input = document.createElement('input');
-    input.setAttribute('name', 'innerinput');
+function handleClick() {
+  // console.log('buttom was clicked');
+  const div = document.getElementById("innerDiv");
+  const button = document.createElement("button");
+  button.textContent = "Click me!!!";
 
-    input.addEventListener('input', function (event) {
-        console.log(event.target.value);
-        handleInput(event);    
-})
+  div.appendChild(button);
 
+  button.addEventListener("click", function (event) {
+    console.log(event.target);
+  });
+  const input = document.createElement("input");
+  input.setAttribute("name", "innerinput");
+
+  input.addEventListener("input", function (event) {
+    console.log(event.target.value);
+    handleInput(event);
+  });
 }
-function handleInput(e){
-    console.log(e.target.value, e.target.name);
-}
-
-function handleOver(e){
-    // const div = document.querySelector('div');
-    // console.log("div =>", div);
-    e.target.style.backgroundColor = "yellow";
-    // console.log(e.target);
+function handleInput(e) {
+  console.log(e.target.value, e.target.name);
 }
 
-function handleLeave(e){
-    e.target.style.backgroundColor = "red";
+function handleOver(e) {
+  // const div = document.querySelector('div');
+  // console.log("div =>", div);
+  e.target.style.backgroundColor = "yellow";
+  // console.log(e.target);
+}
+
+function handleLeave(e) {
+  e.target.style.backgroundColor = "red";
 }
