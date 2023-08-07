f9353b406f1d459b87d0ae8e1c35a5b0f1af3645
Guil-levy <guil.levy12@gmail.com>
2023-08-07 21:27:31 +0300

    1

---
+++ week4/day5/Exercices/ex1.js
@@ -0,0 +1,29 @@
+// // const div = document.querySelector("div")
+// // console.log(div);
+
+// // 2/
+// let name = (document.querySelector(".list").children[1].textContent =
+//   "Richard");
+
+// // 2/3
+
+// let lists = document.querySelectorAll(".list");
+// lists[1].children[1].remove();
+
+// // 2/3
+// lists.forEach((list) => {
+//   list.firstElementChild.textContent = "Guil";
+// });
+
+// // 3/1
+// lists.forEach((list) => {
+//   list.classList.add("student_list");
+// });
+
+// // 3/2
+
+// let uni = document.querySelector("ul");
+// uni.classList.add("university", "attendance");
+
+
+
