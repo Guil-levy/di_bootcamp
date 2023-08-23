a13cccc252de5493586f10d733c6efc7a461d50f
Guil-levy <guil.levy12@gmail.com>
2023-08-23 21:35:00 +0300

    1

---
+++ week7/day3/script.js
@@ -0,0 +1,9 @@
+const arr = [
+    {id:1, name:'iPhone', price:850},
+    {id:2, name:'iPad', price:750},
+    {id:3, name:'iWatch', price:650}
+];
+
+arr.forEach( (element) => {
+    console.log([0]);
+});
