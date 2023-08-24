82505c5fe2ae430e6ab6b18920da243687bcc11c
Guil-levy <guil.levy12@gmail.com>
2023-08-24 21:05:10 +0300

    1

--- week7/day5/DailyChal/script.js
+++ week7/day5/DailyChal/script.js
@@ -1,7 +1,12 @@
-function allTruthy(...values) {
-  return values.every(value => !!value);
-}
-
+function allTruthy() {
+    for (let i = 0; i < arguments.length; i++) {
+      if (!arguments[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
 console.log(allTruthy(true, true, true)); 
 console.log(allTruthy(true, false, true)); 
 console.log(allTruthy(5, 4, 3, 2, 1, 0));
