a5db7d700632602e5356e74e310d0f267f764404
Guil-levy <guil.levy12@gmail.com>
2023-09-08 21:43:46 +0300

    1

--- week9/day5/MiniProject/script.js
+++ week9/day5/MiniProject/script.js
@@ -1,74 +1,71 @@
+
 const form = document.getElementById('weatherForm');
-const getCity= document.getElementById('location');
-const submit = document.getElementById('submit')
+const getCity = document.getElementById('location');
 const apiKey = '6bc236fa8bd5e7e03f83fd8cea3eac74';
-
-const xhr = new XMLHttpRequest();
-
-const weatherCard = document.getElementById('weatherCard');
-const cityNameElement = document.getElementById('cityName');
-const countryNameElement = document.getElementById('countryName');
-const weatherDescriptionElement = document.getElementById('weatherDescription');
-const weatherIconElement = document.getElementById('weatherIcon');
-const temperatureElement = document.getElementById('temperature');
-const humidityElement = document.getElementById('humidity');
-
-const convertToCelsiusButton = document.getElementById('convertToCelsius');
-const convertToFahrenheitButton = document.getElementById('convertToFahrenheit');
-
-
-
+const weatherCardsContainer = document.getElementById('weatherCardsContainer');
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
     const cityName = getCity.value;
 
+    const xhr = new XMLHttpRequest();
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
-    xhr.open('get', apiUrl);
+    xhr.open('GET', apiUrl);
 
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            const data = JSON.parse(xhr.responseText);
+            createWeatherCard(data);
+        } else {
+            console.log(`${xhr.status} : ${xhr.statusText}`);
+        }
+    };
 
-xhr.send();
+    xhr.onerror = () => {
+        console.log("Request failed");
+    };
 
-xhr.onload = () => {
-  if (xhr.status === 200){
-    const data = JSON.parse(xhr.responseText);
-    displayWeatherData(data);
-   }else {console.log(`${xhr.status} : ${xhr.statusText} `);
-}
-}});
+    xhr.send();
+});
 
-xhr.onerror = () => {
-  console.log("Request failed");
-};
+function createWeatherCard(data) {
+    const weatherCard = document.createElement('div');
+    weatherCard.className = 'weather-card';
 
-xhr.onprogress = (event) => {
-  if (event.lengthComputable) {
-    console.log(`Received ${event.loaded}`);
-  } else {
-    console.log(`Received ${event.loaded}`);
-  }
-};
+    const closeButton = document.createElement('span');
+    closeButton.className = 'close-button';
+    closeButton.textContent = 'X';
 
-function displayWeatherData(data) {
+    closeButton.addEventListener('click', () => {
+        weatherCard.remove();
+    });
+
+    const cityNameElement = document.createElement('h2');
     cityNameElement.textContent = data.name;
+
+    const countryNameElement = document.createElement('p');
     countryNameElement.textContent = data.sys.country;
+
+    const weatherDescriptionElement = document.createElement('p');
     weatherDescriptionElement.textContent = data.weather[0].description;
+
+    const weatherIconElement = document.createElement('img');
     weatherIconElement.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    weatherIconElement.alt = 'Weather Icon';
+
+    const temperatureElement = document.createElement('p');
     temperatureElement.textContent = `Temperature: ${data.main.temp} K`;
+
+    const humidityElement = document.createElement('p');
     humidityElement.textContent = `Humidity: ${data.main.humidity}%`;
+    
+    weatherCard.appendChild(closeButton);
+    weatherCard.appendChild(cityNameElement);
+    weatherCard.appendChild(countryNameElement);
+    weatherCard.appendChild(weatherDescriptionElement);
+    weatherCard.appendChild(weatherIconElement);
+    weatherCard.appendChild(temperatureElement);
+    weatherCard.appendChild(humidityElement);
 
-    weatherCard.style.display = 'block';
-
-    // Add event listeners to the conversion buttons
-    convertToCelsiusButton.addEventListener('click', () => {
-        const temperatureInKelvin = data.main.temp;
-        const temperatureInCelsius = temperatureInKelvin - 273.15; // Convert K to °C
-        temperatureElement.textContent = `Temperature: ${temperatureInCelsius.toFixed(2)} °C`;
-    });
-
-    convertToFahrenheitButton.addEventListener('click', () => {
-        const temperatureInKelvin = data.main.temp;
-        const temperatureInFahrenheit = (temperatureInKelvin * 9/5) - 459.67; // Convert K to °F
-        temperatureElement.textContent = `Temperature: ${temperatureInFahrenheit.toFixed(2)} °F`;
-    });
+    weatherCardsContainer.appendChild(weatherCard);
 }
