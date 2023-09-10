43dfe98833422108044724227a41224bdc703264
Guil-levy <guil.levy12@gmail.com>
2023-09-10 21:00:22 +0300

    1

---
+++ week10/day1/script.js
@@ -0,0 +1,42 @@
+// function countVowels(sentence) {
+//     const vowels = ['a', 'e', 'i', 'o', 'u'];
+//     let count = 0;
+  
+//     sentence.toLowerCase().split('').forEach(char => {
+//         if (vowels.includes(char)) {
+//           count++;
+//         }
+//       });
+//     return count;
+//   }
+//   const sentence = "Hi, my name is Guil, and I'm 38 years old";
+//   const vowelCount = countVowels(sentence);
+//   console.log(vowelCount); 
+
+//   const sentence2 = "It's raining man, Halleluya";
+//   const vowelCount2 = countVowels(sentence2);
+//   console.log (vowelCount2)
+
+// ------------------------
+
+const compareToTen = (num) =>{
+   
+    return new Promise((resolve, reject)=>{
+    if(num>10) 
+    { resolve (`${num} is greater than 10`)}
+    }else{
+    return reject(`${num} is less than 10`)
+        }    
+    });
+}
+
+
+compareToTen(11)
+    .then(resolve =>
+{
+console.log(resolve);
+})  
+.catch(err =>{console.log(err);})
+  
+  
+  
