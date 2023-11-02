b117ef5edc6bfc32b69c9b0cc40f38fbf77e6290
Guil-levy <guil.levy12@gmail.com>
2023-11-02 19:36:23 +0200

    1

---
+++ week14/day5/blog/src/components/ToDo.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      currentTask: '',
+    };
+  }
+
+  renderToDos = (e) => {
+    const { todos, currentTask } = this.state;
+    if (e.key =="Enter"){
+    if (currentTask !== '') {
+      this.setState({
+        todos: [...todos, currentTask],
+        currentTask: '',
+      });
+    }}
+  };
+
+  handleInputChange = (e) => {
+    
+    this.setState({ currentTask: e.target.value });
+  };
+
+  deleteTask = (pos) => {
+    const updatedTodos = this.state.todos.filter((task, i) => i !== pos);
+    this.setState({ todos: updatedTodos });
+  };
+
+  render() {
+    return (
+      <div>
+          <ul>
+          {this.state.todos.map((task, i) => (
+            <li key={i} onClick={() => this.deleteTask(i)}>
+              Task {i + 1}: {task}
+            </li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          value={this.state.currentTask}
+          onInput={this.handleInputChange}
+          placeholder="Enter your task here"
+          onKeyDown={this.renderToDos}
+        />
+       
+        
+        
+      </div>
+    );
+  }
+}
+
+export default Todo;
+
