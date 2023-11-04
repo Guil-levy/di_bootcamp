db8a438fd91d233ce579d4cdded22fb1466c0c7d
Guil-levy <guil.levy12@gmail.com>
2023-11-04 14:51:29 +0200

    1

--- week13/day4/blog/src/Color.js
+++ week13/day4/blog/src/Color.js
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect  } from "react";
 
 const Color = () => {
     const [favoriteColor, setFavoriteColor] = useState("red");
-  
+    
+    useEffect(() => {
+      setFavoriteColor("yellow");
+    }, []);
+    const clickToChange=()=>{
+      setFavoriteColor("blue");
+    }
+
     return (
       <div>
         <h1>My Favorite Color is {favoriteColor}</h1>
+        <button onClick={clickToChange}>Click to change color to blue</button>
       </div>
     );
   };
