98bbe2b8aa990f01f55e912d7bf957883d94f38a
Guil-levy <guil.levy12@gmail.com>
2023-12-21 18:52:45 +0200

    1

---
+++ week20/day3/calculator.py
@@ -0,0 +1,5 @@
+# from operators import addOperator, divideOperator
+import operators as op
+
+print(op.addOperator(1,2))
+print(op.divideOperator(1,2))
---
+++ week20/day3/operators.py
@@ -0,0 +1,5 @@
+def addOperator(a,b):
+    return a+b
+
+def divideOperator(a,b):
+    return a/b
