b14b7f4f840aa82e0bba30b9f07eb5a84f30da43
Guil-levy <guil.levy12@gmail.com>
2023-12-25 19:41:00 +0200

    1

--- week19/day2/Matrix dailyChal.py
+++ week19/day2/Matrix dailyChal.py
@@ -1,52 +1,26 @@
-# # final_matrix = [list(row) for in my_matrix.split('\n')]
-# # print(final_matrix)
-# my_matrix = '''
-#
-#     7ii
-#     Tsx
-#     h%?
-#     i #
-#     sM
-#     $a
-#     #t%
-#     ^r!
-#
-# '''
-#
-# print(my_matrix.split('\n'))
-# final_matrix = [list(row) for row in my_matrix.split('\n')]
-# print(final_matrix)
-# #
-#
-# #
-# #
-# # final_result = ""
-# # print(list(final_matrix))
-# # for col_index in range(len(final_matrix[0]):
-# #     for row_index in range(len(final_matrix)):
-# #         if final_matrix[row_index][col_index].isalpha():
-# #             final_result += final_matrix[row_index][col_index]
-# #         elif final_result !="" and final_result[-1] != " ":
-# #             final_result += " "
-# #
-# #
-# # print(final_result)
-#
-# people = ["Rick", "Morty", "Beth", "Jerry", "Snowball"]
-# def people_len (s):
-#     if len(people(i))<= 4 :
-#         return people
-# print(list(people_len))
-sum_of_number = 0
-my_list = [2,3,1,2,"four",42,1,5,3,"imanumber"]
+#not working . didn't have time to write it all during class nor understand it
+my_matrix = '''
 
-flag = False
+    7ii
+    Tsx
+    h%?
+    i #
+    sM
+    $a
+    #t%
+    ^r!
 
-my_new_list = while my_list != False :
-    try:
-       sum_of_number my_list[i] + my_list[i+1]
-        flag = True
-    except:
-        my_list[i] = type(str)
+'''
 
-print (my_new_list)
+final_matrix = [list(row) for row in my_matrix.split('\n') if row.strip()]
+print(final_matrix)
+
+final_result = ""
+for col_index in range(len(final_matrix[0])):
+    for row_index in range(len(final_matrix)):
+        if final_matrix[row_index][col_index].isalpha():
+            final_result += final_matrix[row_index][col_index]
+        elif final_result != "" and final_result[-1] != " ":
+            final_result += " "
+
+print(final_result)
