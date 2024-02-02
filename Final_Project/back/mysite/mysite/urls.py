1dc6e27a62062186ba7dbfbf067643ca1add31cd
Guil-levy <guil.levy12@gmail.com>
2024-02-02 16:59:18 +0200

    1

---
+++ Final_Project/back/mysite/mysite/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from api.views import book
+# from core.views import index
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("book/", book, name="book"),
+]
