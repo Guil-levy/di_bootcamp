50b11c524b89a2730cea333a8e19fccd4edcab1d
Guil-levy <guil.levy12@gmail.com>
2024-02-03 17:02:09 +0200

    1

--- Final_Project/back/mysite/mysite/settings.py
+++ Final_Project/back/mysite/mysite/settings.py
@@ -1,6 +1,7 @@
 
 from pathlib import Path
-
+import os
+import core
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,11 +27,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # "corsheaders",
+
     'rest_framework',
+
     'Games',
     'User',
     'UserGames',
-    
+
     'api',
     'core',
 ]
@@ -50,7 +54,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'gsteam')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,7 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'gsteam', 'build','static'),
+CORS_ORIGIN_ALLOW_ALL = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- Final_Project/back/mysite/mysite/urls.py
+++ Final_Project/back/mysite/mysite/urls.py
@@ -2,9 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 from api.views import book
-# from core.views import index
+from core.views import index
 
 urlpatterns = [
+    path("", index, name="index"),
     path('admin/', admin.site.urls),
     path("book/", book, name="book"),
 ]
