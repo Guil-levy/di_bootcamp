47d8640a5a0d427efe5ae9a08fd814e4c1610117
Guil-levy <guil.levy12@gmail.com>
2024-04-13 19:24:33 +0300

    1

---
+++ afterschoolW1/day1/js1.js
@@ -0,0 +1,11 @@
+// SPLIT, IndexOf
+
+// console.log('To be or not to be'.indexOf('To'));
+// console.log('To be or not to be'.indexOf(' '));
+// console.log('To be or not to be'.indexOf('o', 2));
+// console.log('To be or not to be'.indexOf('be', 4));
+// console.log('To be or not to be'.indexOf('to'));
+// console.log('To be or not to be'.indexOf('B'));
+// console.log('To be or not to be'.indexOf('', 9));
+
+
